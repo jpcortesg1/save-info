1ffba16e25c6ea4c41b94f0473a42f42989ee7c0
jpcortesg1 <jpcortesg@eafit.edu.co>
2022-03-31 07:32:31 -0500

    create description

--- client/src/components/info/description/Description.jsx
+++ client/src/components/info/description/Description.jsx
@@ -1,11 +1,34 @@
-import "./description.css"
+import { useState, useEffect } from "react";
+import "./description.css";
 
-const Description = () => {
+const Description = ({ info, id, deleteInfo }) => {
+  const [description, setDescription] = useState(info.description);
+
+  useEffect(() => {
+    info.description = description;
+  }, [description, info]);
+
+  const handleDelete = () => {
+    deleteInfo(id);
+  };
+
   return (
-    <div className="description">
-      Description
+    <div className="infoDescription">
+      <div className="infoDescriptionWrapper">
+        <textarea
+          className="infoDescriptionWrapperContent"
+          placeholder="Writte your description"
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <div className="infoDescriptionActions">
+          <i
+            className="fa-solid fa-trash infoDescriptionAction"
+            onClick={handleDelete}
+          ></i>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Description
+export default Description;
